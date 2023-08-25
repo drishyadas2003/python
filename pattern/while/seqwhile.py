dd9be7b98f68c361a204e3efd753b2d1af1f8aa3
drishya <drishyadas32@gmail.com>
2023-08-25 18:00:41 +0530

    adding

---
+++ pattern/while/seqwhile.py
@@ -0,0 +1,50 @@
+# l=[11,2,43,4,5]
+# i=0
+# while i<len(l):
+#     print(l[i])
+#     i+=1
+
+
+# a="computer"
+# i=0
+# while i<len(a):
+#     print(a[i])
+#     i+=1
+
+# l=[11,22,3,4,55,6,7,24]
+# i=0
+# l1=[]
+# while i<len(l):
+#     if l[i]%2==0:
+#         l1.append(l[i])
+#     i+=1
+# print(l1)  
+# sum=sum(l1) 
+# print(sum)
+
+
+# a=[2,3,4,10,20,9,6,15]
+# i=0
+
+# l1=[]
+# while i<len(a):
+#     if a[i]%3==0 or a[i]%5==0:
+#         print(a[i])
+#         l1.append(a[i])
+#     i+=1
+# sum=sum(l1)
+# print("sum is:",sum)
+
+# c=0
+# a=input("enter any number:")
+# i=0
+# while i<len(a):
+#     c+=1
+#     i+=1
+#    
+
+# print(c)
+
+
+
+
--- pattern/while/while.py
+++ pattern/while/while.py
@@ -23,8 +23,23 @@
 # print(l1)
 # print(l2)    
 
-i=1
-a=int(input("enter a no:"))
-while i<a+1:
-    print(i,"x",a,"=",i*a)
-    i+=1
+# i=1
+# a=int(input("enter a no:"))
+# while i<a+1:
+#     print(i,"x",a,"=",i*a)
+#     i+=1
+
+
+
+# a=int(input("enter 1st no:"))
+# b=int(input("enter 2nd no:"))
+# l=[]
+# i=1
+# while i<a or i<b:
+#     if a%i==0 and b%i==0:
+#         print(i)
+#         l.append(i)
+#     i+=1
+# max(l)
+# gcd=max(l)
+# print("gcd is :",gcd)
