7367a935ac59fdf6f36d847c436d98e23343615c
drishya <137509505+drishyadas2003@users.noreply.github.com>
2023-09-14 05:16:42 -0700

    adding

---
+++ builtinmodules.py
@@ -0,0 +1,17 @@
+# import math as m
+# print(m.factorial(5))
+# print(m.sqrt(25))
+# print(m.gcd(12,6))
+# print(m.floor(5.7))
+# print(m.pow(2,5))
+# print(m.ceil(5.8))
+# print(m.fsum([1,2,3,4,5]))
+# print(m.fsum((1,2,3,4,5)))
+# print(m.fsum({1,2,3,4,5}))
+
+
+from datetime import*
+print(datetime.now())
+print(date.today())
+d=date(2003,4,11)
+print(d)
---
+++ mymodule.py
@@ -0,0 +1,25 @@
+# def sample(a,b):
+#     s=a+b
+#     print(s)
+
+
+# def add(a,b):
+#     addition=a+b
+#     print(addition)
+# def sub(a,b):
+#     substraction=a-b
+#     print(substraction)
+# def mul(a,b):
+#     multiplication=a*b
+#     print(multiplication)
+# def div(a,b):
+#     division=a/b
+#     print(division)
+
+
+
+class A:
+    name="abc"
+    def dis(self):
+        print("hello")
+ob=A()
