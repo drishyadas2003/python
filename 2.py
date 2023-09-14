3c240d3fb3aa3409e9be7e1913e3d46cab5a9f85
drishya <137509505+drishyadas2003@users.noreply.github.com>
2023-09-14 14:19:35 +0530

    adding

---
+++ 2.py
@@ -0,0 +1,34 @@
+# class A:
+#     def __init__(self,n,m):
+#         self.num1=n
+#         self.num2=m
+#     def add(self):
+#         print(self.num1+self.num2)
+# ob=A(10,20)
+# ob.add()
+        
+
+#single inheritance
+# class A:
+#     def dis(self):
+#         print("hai")
+# class B(A):
+#     pass
+# ob=B()
+# ob.dis()
+
+
+# #multiple inheritance
+# class A:
+#     def dis(self):
+#         print("hai")
+# class B:
+#     def display(self):
+#         print(10)
+# class C(A,B):
+#     pass
+# ob=C()
+# ob.dis()
+# ob.display()
+
+
