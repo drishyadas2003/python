50af388b2f857ad8d662d9a7cb33b7e5f037da8d
drishya <137509505+drishyadas2003@users.noreply.github.com>
2023-09-20 17:59:50 +0530

    adding

---
+++ exception.py
@@ -0,0 +1,37 @@
+# a=5
+# try:
+#     print(a/b)
+# except:
+#     print("error happend")
+# print("hello")
+
+
+# a=5
+# try:
+#     print(a/0)
+# except:
+#     print("exception zerodivisor")
+
+# try:
+#     print(a)
+# except:
+#     print("error happend!!")
+# else:
+#     print("valid")
+# finally:
+#     print("END")
+
+# try:
+#     z=int(input("::"))
+# except:
+#     print("invalid")
+
+
+
+try:
+    a=int(input("::"))
+    b=int(input("::"))
+    c=a+b
+    print(c)
+except:
+    print("invalid")
--- problems.py
+++ problems.py
@@ -1,17 +1,17 @@
-# a=str(input("enter a string:"))
-# v=['a','e','i','o','u']
-# b=[]
-# char=" "
-# for i in a:
-#     if i in v:
-#         b.append(i)
-# rev=b[::-1]
-# l=list(a)
-# l[1]=rev[0]
-# l[-1]=rev[1]
-# for i in l:
-#     char+=i
-# print(char)
+# # a=str(input("enter a string:"))
+# # v=['a','e','i','o','u']
+# # b=[]
+# # char=" "
+# # for i in a:
+# #     if i in v:
+# #         b.append(i)
+# # rev=b[::-1]
+# # l=list(a)
+# # l[1]=rev[0]
+# # l[-1]=rev[1]
+# # for i in l:
+# #     char+=i
+# # print(char)
 
 
 a=str(input("enter a string:"))
@@ -19,21 +19,42 @@ v=['a','e','i','o','u']
 b=[]
 index=[]
 ind=[]
+count=0
+c=0
+l1=[]
+l2=[]
 char=" "
 for i in a:
     if i in v:
         b.append(i)
         index=b.index(i)
-        print(index)
-rev=b[::-1]
-for j in rev:
-    ind=rev.index(j)
-    print(ind)
+        l1.append(index)
+        count+=1
+        l2.append(count)
+print(l1)
+print(l2)
+        
+# # rev=b[::-1]
+# # for j in rev:
+# #     ind=rev.index(j)
+# #     c+=1
+# #     l2.append(c)
+# # print(l2)
+# # l=list(a)
 
-# l=list(a)
-# l[1]=rev[0]
-# l[-1]=rev[1]
-# for i in l:
-#     char+=i
-# print(char)
+
+# # l=list(a)
+# # l[1]=rev[0]
+# # l[-1]=rev[1]
+# # for i in l:
+# #     char+=i
+# # print(char)
+
+
+
+
+
+# # a=int(input("enter a number:"))
+# # b=str(a)
+# # print(b[::-1])
 
